7a89958e57b08e33267cfd2d66995d258a17e5a9
Alberto Miranda <alberto.miranda@opendeusto.es>
2020-04-19 17:29:39 +0200

    html logix

--- public/js/twNudity.js
+++ public/js/twNudity.js
@@ -1,1 +1,40 @@
 //CONTROLADOR PUBLICO
+//var config = require('../../components/twitter/twitterCtlr.js');
+
+//lama a la api de tw
+function analizeUser(arroba) {
+	//data = getUserData(arroba);
+	if (document.getElementById("resultado").style.display=="none") {
+		if (false/*data === null*/) {		
+			var div = document.getElementById("error");
+			div.style.display = "block";
+		} else {
+			var div = document.getElementById("load");
+			div.style.display = "block";
+			var div = document.getElementById("barra");
+			val=0;
+			var interid = setInterval(function () {
+				console.log(val);	
+		        if (val <= 101) {
+		            div.style.width = val+"%";
+		            val++;
+		        }
+		        if (val==101) {
+		        	clearInterval(interid);
+		        	setTimeout(showRes(), 15000);
+		        }
+	   		}, 50);
+	   		//mientras la progress tira hacer el proceso de gestionar to
+		}
+	} else {
+		var div = document.getElementById("resultado");
+		div.style.display = "none";
+		document.getElementById("barra").style.width = "0%";
+		analizeUser();
+	}
+}
+
+function showRes() {
+	var div = document.getElementById("resultado");
+	div.style.display = "block";
+}
