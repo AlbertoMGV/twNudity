56d99d05a5dc3d6e7656e2f159bb091b465bdbe4
Alberto Miranda <alberto.miranda@opendeusto.es>
2020-05-19 12:42:06 +0200

    code cleaned

--- routes/index.js
+++ routes/index.js
@@ -19,22 +19,6 @@ const nsfw = require('nsfwjs')
 
 var upload = multer()
 
-const convert = async (img) => {
-  // Decoded image in UInt8 Byte array
-  const image = await jpeg.decode(img, true)
-
-  const numChannels = 3
-  const numPixels = image.width * image.height
-  const values = new Int32Array(numPixels * numChannels)
-
-  for (let i = 0; i < numPixels; i++)
-    for (let c = 0; c < numChannels; ++c)
-      values[i * numChannels + c] = image.data[i * 4 + c]
-
-  return tf.tensor3d(values, [image.height, image.width, numChannels], 'int32')
-}
-
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'twNudity' });
