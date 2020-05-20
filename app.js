5221205b487789f2bf78019ef9d27e488720d9b9
Alberto Miranda <alberto.miranda@opendeusto.es>
2020-05-20 12:16:23 +0200

    switch fun loc

--- app.js
+++ app.js
@@ -19,23 +19,6 @@ var userRouter = require('./routes/users');
 
 var app = express();
 
-
-
-const convert = async (img) => {
-  // Decoded image in UInt8 Byte array
-  const image = await jpeg.decode(img, true)
-
-  const numChannels = 3
-  const numPixels = image.width * image.height
-  const values = new Int32Array(numPixels * numChannels)
-
-  for (let i = 0; i < numPixels; i++)
-    for (let c = 0; c < numChannels; ++c)
-      values[i * numChannels + c] = image.data[i * 4 + c]
-
-  return tf.tensor3d(values, [image.height, image.width, numChannels], 'int32')
-}
-
 const load_model = async () => {
   _model = await nsfw.load()
 }
--- routes/index.js
+++ routes/index.js
@@ -82,4 +82,19 @@ router.post('/', function(req, res, next) {
 
 });
 
+const convert = async (img) => {
+  // Decoded image in UInt8 Byte array
+  const image = await jpeg.decode(img, true)
+
+  const numChannels = 3
+  const numPixels = image.width * image.height
+  const values = new Int32Array(numPixels * numChannels)
+
+  for (let i = 0; i < numPixels; i++)
+    for (let c = 0; c < numChannels; ++c)
+      values[i * numChannels + c] = image.data[i * 4 + c]
+
+  return tf.tensor3d(values, [image.height, image.width, numChannels], 'int32')
+}
+
 module.exports = router;
